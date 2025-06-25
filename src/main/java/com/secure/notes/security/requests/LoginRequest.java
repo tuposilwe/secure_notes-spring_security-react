a685b4964e16ce5941b9d886d973537ae4e43ce9
tuposilwe <rudigermkondya@gmail.com>
2025-06-25 19:48:11 +0300

    AddedSign-in Flow  JWT Authentication with Spring Security

---
+++ src/main/java/com/secure/notes/security/requests/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.secure.notes.security.requests;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class LoginRequest {
+    private String username;
+
+    private String password;
+
+}
---
+++ src/main/java/com/secure/notes/security/requests/LoginResponse.java
@@ -0,0 +1,22 @@
+package com.secure.notes.security.requests;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+public class LoginResponse {
+    private String jwtToken;
+
+    private String username;
+    private List<String> roles;
+
+    public LoginResponse(String username, List<String> roles, String jwtToken) {
+        this.username = username;
+        this.roles = roles;
+        this.jwtToken = jwtToken;
+    }
+
+}
