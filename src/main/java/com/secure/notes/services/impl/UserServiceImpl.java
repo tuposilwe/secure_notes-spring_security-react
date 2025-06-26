830918c45b269cb5b12a0789d83455157ca99a6c
tuposilwe <rudigermkondya@gmail.com>
2025-06-26 10:29:43 +0300

    Added Getting User Details Endpoint

--- src/main/java/com/secure/notes/services/impl/UserServiceImpl.java
+++ src/main/java/com/secure/notes/services/impl/UserServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -65,5 +66,11 @@ public class UserServiceImpl implements UserService {
         );
     }
 
+
+    @Override
+    public User findByUsername(String username) {
+        Optional<User> user = userRepository.findByUserName(username);
+        return user.orElseThrow(() -> new RuntimeException("User not found with username: " + username));
+    }
 
 }
