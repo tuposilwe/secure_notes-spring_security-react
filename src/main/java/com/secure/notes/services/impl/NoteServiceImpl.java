19967c563eb897db29fed5eb6fde009382438214
tuposilwe <rudigermkondya@gmail.com>
2025-06-27 10:45:56 +0300

    Added Recording Audit Events

--- src/main/java/com/secure/notes/services/impl/NoteServiceImpl.java
+++ src/main/java/com/secure/notes/services/impl/NoteServiceImpl.java
@@ -2,6 +2,7 @@ package com.secure.notes.services.impl;
 
 import com.secure.notes.models.Note;
 import com.secure.notes.repositories.NoteRepository;
+import com.secure.notes.services.AuditLogService;
 import com.secure.notes.services.NoteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +14,14 @@ import java.util.List;
 @RequiredArgsConstructor
 public class NoteServiceImpl implements NoteService {
     private final NoteRepository noteRepository;
+    private AuditLogService auditLogService;
 
     @Override
     public Note createNoteForUser(String username, String content) {
         Note note = new Note();
         note.setContent(content);
         note.setOwnerUsername(username);
+        auditLogService.logNoteCreation(username,note);
         return noteRepository.save(note);
     }
 
@@ -28,13 +31,18 @@ public class NoteServiceImpl implements NoteService {
                 () -> new RuntimeException("Note note found")
         );
         note.setContent(content);
-
+        auditLogService.logNoteUpdate(username,note);
         return noteRepository.save(note);
     }
 
     @Override
     public void deleteNoteForUser(Long noteId, String username) {
-      noteRepository.deleteById(noteId);
+        Note note = noteRepository.findById(noteId).orElseThrow(
+                () -> new RuntimeException("Note not found")
+        );
+        auditLogService.logNoteDeletion(username,noteId);
+        noteRepository.delete(note);
+
     }
 
     @Override
